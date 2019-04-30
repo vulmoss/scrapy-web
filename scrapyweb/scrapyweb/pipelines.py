7be22bc103df17173aadc8c7d0ae725afc5afdb9
anondba <yaokevin1987@gamil.com>
2019-04-30 17:41:05 +0900

    first_pipe

--- scrapyweb/scrapyweb/pipelines.py
+++ scrapyweb/scrapyweb/pipelines.py
@@ -5,7 +5,18 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from sqlalchemy.orm import sessionmaker
+from scrapyweb.models import Course
 
 class ScrapywebPipeline(object):
     def process_item(self, item, spider):
         return item
+
+    def open_spider(self,spider):
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+
+    def close_spider(self,spider):
+        self.session.commit()
+        self.session.close()
+
